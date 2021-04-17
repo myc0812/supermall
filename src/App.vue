09019a0403e8e96e837a1121bd64298c9aa16212
myc0812 <mayichai123@gmail.com>
2021-04-17 11:19:47 +0800

    introducing css tabbars into the project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
+    <main-tab-bar/>
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
 
+<script>
+import MainTabBar from "./components/content/maintabbar/MainTabBar";
+
+export default {
+  components: {MainTabBar}
+}
+</script>
+
+<style lang="less">
+@import "assets/css/base.css";
 </style>
---
+++ src/network/request.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export function reqeust(config) {
+  const instance = axios.create({
+    baseURL: "http://152.136.185.210:7878/api/m5",
+    timeout: 50000
+  });
+
+  instance.interceptors.request.use(
+    config => {
+      return config;
+    },
+    error => {
+      console.log(error);
+    }
+  );
+
+  instance.interceptors.response.use(
+    res => {
+      return res.data;
+    },
+    error => {
+      console.log(error);
+    }
+  );
+}
